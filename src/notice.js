b0eea4c38d28042e4858b734430ae106bb977fa5
Nagi65536 <87361015+Nagi65536@users.noreply.github.com>
2022-05-23 09:14:51 +0900

    Update notice.js

--- src/notice.js
+++ src/notice.js
@@ -6,20 +6,35 @@ db.run("CREATE TABLE if not exists notice \
     (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, guildId TEXT, channelId TEXT)");
 
 exports.noticeAdd = function (message) {
+    const date = moment().local().format('YYYY-MM-DD HH:mm:ss');
     const guildId = message.guild.id;
     const channelId = message.channel.id;
 
-    db.each(`SELECT id FROM notice WHERE guildId = ${guildId} AND channelId = ${channelId}` , (err, row) => {
+    db.each(`SELECT id FROM notice WHERE guildId = ${guildId}` , (err, row) => {
         if (row) db.run(`DELETE from data WHERE id = ${row.id}`)
     });
 
     db.run(`INSERT INTO notice(date, guildId, channelId) \
-        VALUES("${date}", ${guildId}, "${channelId}", "${gamemode}", "${colorcode}")`);
+        VALUES("${date}", ${guildId}, "${channelId}")`);
 
     message.channel.send('ここにお知らせを送ります')
+}
+
+exports.noticeDel = function (guildID) {
+    const guildId = message.guild.id;
+    const channelId = message.channel.id;
+    
+    db.each(`SELECT id FROM notice WHERE guildId = ${guildId}` , (err, row) => {
+        if (row) db.run(`DELETE from notice WHERE id = ${row.id}`)
+    });
 }
 
 exports.noticec = function (message) {
     const guildId = message.guild.id;
     const channelId = message.channel.id;
+    
+    db.each(`SELECT * FROM notice WHERE guildId = ${guildId}` , (err, row) => {
+        if (row) client.channels.cache.get(row.channelId)
+            .send('お知らせの内容です');
+    });
 }
