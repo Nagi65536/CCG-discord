5226d82abbbce3540efa8b1b6bd0281fb5125fbe
Nagi65536 <87361015+Nagi65536@users.noreply.github.com>
2022-05-23 11:33:29 +0900

    Create training.js

---
+++ src/training.js
@@ -0,0 +1,27 @@
+const moment = require('moment');
+const sqlite3 = require("sqlite3");
+const generatecc = require("./generatecc.js");
+const db = new sqlite3.Database("./main.db");
+
+
+exports.trainingc = function (message) {
+    try {
+        const date = moment().local().format('YYYY-MM-DD HH:mm:ss');
+        const guildId = message.guild.id;
+        const channelId = message.channel.id;
+        const gamemode = 'training';
+        const colorcode = generatecc.generateCC()
+        
+        db.run(`DELETE from data WHERE guildId="${guildId}" AND channelId="${channelId}"`)
+
+        db.run(`INSERT INTO data(date, guildId, channelId, gamemode, colorcode) \
+            VALUES("${date}", ${guildId}, "${channelId}", "${gamemode}", "${colorcode}")`);
+
+        message.channel.send(
+            `\\\\\\ ONE SHOT START!!! ///\nカラーコードを一発で当てよ!`,
+            { files: [`./images/${colorcode}.png`] }
+        )
+    } catch (e) {
+        message.channel.send('エラーが発生しました')
+    }
+}
